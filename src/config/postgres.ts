8596b583ab4ca7b14a7bb37d49936323cc948c99
Eric Sison <ericsison.dev@gmail.com>
2024-10-04 16:00:37 +0800

    updated postgres and redis config to retrieve values from environment

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -1,12 +1,13 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { appConfig } from "./environment";
 
 const client = postgres({
-  host: "",
-  port: 5432,
-  username: "",
-  password: "",
-  database: "",
+  host: appConfig.dbHost,
+  port: Number(appConfig.dbPort),
+  username: appConfig.dbUser,
+  password: appConfig.dbPassword,
+  database: appConfig.dbName,
   max: 20,
   idle_timeout: 20, // close connection the has been idle for 20 secs
   max_lifetime: 60 * 60, // close connections that have existed for more than 1 hour
--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,11 +1,12 @@
 import { Redis } from "ioredis";
+import { appConfig } from "./environment";
 
-export const redisStore = new Redis(6379, "", {
-  password: "",
+export const redisStore = new Redis(appConfig.redisPort, appConfig.redisHost, {
+  password: appConfig.redisPassword,
   enableReadyCheck: true,
   maxRetriesPerRequest: 5,
   retryStrategy: (times) => Math.min(times * 50, 500),
 });
 
 // TODO: add production logger
-redisStore.on("error", (err) => console.log(err));
+redisStore.on("error", (err) => console.error(err));
