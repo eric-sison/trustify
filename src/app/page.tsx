558f55283781ec523ff1cde246746b19f1f4567c
Eric Sison <ericsison.dev@gmail.com>
2024-10-04 15:35:39 +0800

    initial project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@trustify/components/ui/Button";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Button>Login</Button>
+    </div>
+  );
+}
