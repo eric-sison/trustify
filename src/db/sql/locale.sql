a8dfffdcb8c6b4f56eda07cfc205a58f83f02b0c
Eric Sison <ericsison.dev@gmail.com>
2024-10-04 21:15:24 +0800

    added raw sql queries for locale and timezone

---
+++ src/db/sql/locale.sql
@@ -0,0 +1,2 @@
+-- use this query for setting the user locale
+SELECT colliculocale FROM pg_collation WHERE colliculocale IS NOT NULL
--- src/db/sql/timezone.sql
+++ src/db/sql/timezone.sql
@@ -1,3 +1,6 @@
 SELECT * FROM pg_timezone_names; -- show all available timezones
 
 SET timezone = 'Asia/Manila'; -- set current timezone
+
+-- use this query for setting the user zoneinfo
+SELECT name FROM pg_timezone_names;
