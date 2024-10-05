e4f77c7e156ef43c730b866aa6b1d043a259a0db
Eric Sison <ericsison.dev@gmail.com>
2024-10-05 22:29:54 +0800

    added logger middleware

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,12 @@
+import { logger } from "hono/logger";
 import { authorizationHandler } from "@trustify/core/handlers/authorization";
 import { discoveryHandler } from "@trustify/core/handlers/discovery";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 const app = new Hono().basePath("/api");
+
+app.use(logger());
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app
