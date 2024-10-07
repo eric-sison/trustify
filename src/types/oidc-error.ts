4aaa027ab903d62b98ad021595a05f418719fb8f
Eric Sison <ericsison.dev@gmail.com>
2024-10-07 10:10:27 +0800

    added stack field in ErrorDetails type

--- src/types/oidc-error.ts
+++ src/types/oidc-error.ts
@@ -4,17 +4,20 @@ type ErrorDetails = {
   error: string;
   message: string;
   status: StatusCode;
+  stack?: string;
 };
 
 export class OidcError extends Error {
   public status: StatusCode;
   public errorType: string;
 
-  constructor({ error, message, status }: ErrorDetails) {
+  constructor({ error, message, status, stack }: ErrorDetails) {
     super(message); // Calls the built-in Error constructor and sets the message
     this.name = "OidcError"; // Sets the error name
     this.status = status; // Adds a custom status code
     this.errorType = error; // Custom error type
+    this.stack = stack;
+
     Object.setPrototypeOf(this, new.target.prototype); // Fixes instanceof checks
   }
 }
