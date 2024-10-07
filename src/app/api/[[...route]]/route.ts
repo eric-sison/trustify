99545392966f54b2cb6c6d760030bb5d6e8cec12
Eric Sison <ericsison.dev@gmail.com>
2024-10-07 10:13:11 +0800

    added notFound and onError middlewares and added the test route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,15 +3,23 @@ import { authorizationHandler } from "@trustify/core/handlers/authorization";
 import { discoveryHandler } from "@trustify/core/handlers/discovery";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { notFound } from "@trustify/core/middlewares/not-found";
+import { onError } from "@trustify/core/middlewares/on-error";
+import { testHander } from "@trustify/core/handlers/test";
 
 const app = new Hono().basePath("/api");
 
 app.use(logger());
 
+app.notFound(notFound);
+
+app.onError(onError);
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app
   .route("/v1/.well-known/openid-configuration", discoveryHandler)
-  .route("/v1/authorization", authorizationHandler);
+  .route("/v1/authorization", authorizationHandler)
+  .route("/v1/test", testHander);
 
 export const GET = handle(app);
 export const POST = handle(app);
